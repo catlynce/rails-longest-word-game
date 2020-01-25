d140d98de61f7be5afb62ebcbd815ad7c7b3654d
Nuno Martins <nuno@alvesmartins.eu>
2020-01-25 16:34:25 +0000

    Solve the game

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
+
+gem 'rest-client'
+
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
@@ -38,7 +41,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  # gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-byebug'
 end
 
 group :development do
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,34 @@
 class GamesController < ApplicationController
   def new
+    @letters = []
+    10.times { @letters << ('a'...'z').to_a.sample }
   end
 
   def score
+    @word = params[:word]
+    @letters = params[:letters].split(' ')
+
+    found = false
+    @word.split('').each do |letter|
+      found = @letters.none? letter
+    end
+    @found = found
+
+    if found
+      @answer = "Sorry, but #{@word} can't be built out of #{@letters.join(', ')}"
+    else
+      url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+
+      response = RestClient.get(url)
+      json = JSON.parse(response)
+
+      if json['found']
+        @answer = "Congratulations! #{@word} is a valid English word!"
+      else
+        @answer = "Sorry but #{@word} does not seem to be a valid English word..."
+      end
+
+    end
   end
+
 end
